d62e91f9752132da212057d69f5550c468fd0697
marcosribeirodacunha <mrcmarcos100@gmail.com>
2020-11-27 12:03:50 -0300

    Finish home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,82 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useEffect, useState } from 'react';
+import { MdChevronRight } from 'react-icons/md';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-import { Container } from './styles';
+import { subMonths } from 'date-fns';
 
-const Home: React.FC = () => {
+import Exam from '../../interfaces/exam';
+import api from '../../services/api';
+import { Container, Table } from './styles';
+
+const EmployeesList: React.FC = () => {
+  const history = useHistory();
+
+  const [exams, setExams] = useState<Exam[]>([]);
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const { data } = await api.get('exams', {
+          params: {
+            due_start: subMonths(new Date(), 1),
+            due_end: new Date(),
+          },
+        });
+        setExams(data);
+      } catch (error) {
+        if (error.response) toast.error(error.response.data.message);
+        else {
+          toast.error(
+            'Um erro inexperado ocorreu. Por favor, tente mais tarde!'
+          );
+        }
+      }
+    }
+
+    loadData();
+  }, []);
+
+  function handleNavigateToDetails(id: string) {
+    history.push(`exames/${id}`);
+  }
+
+  if (!exams) return <h1>Loading...</h1>;
+
   return (
     <Container>
-      <h1>Home</h1>
-      <Link to="/funcoes">Jobs</Link>
+      <h1>Exames a vencer</h1>
+
+      <Table>
+        <header>
+          <strong>Funcionário</strong>
+          <strong>Tipo de exame</strong>
+          <strong>Categoria</strong>
+          <strong>Vencimento</strong>
+        </header>
+        <main>
+          {exams.length === 0 ? (
+            <p>Nenhum exame a vencer no ultimo mês</p>
+          ) : (
+            exams.map(exam => (
+              <div
+                key={exam.id}
+                onClick={() => handleNavigateToDetails(exam.id)}
+              >
+                <p>{exam.employee.name}</p>
+                <p>{exam.type.name}</p>
+                <p>{exam.category.name}</p>
+                <p>{exam.dueDate}</p>
+                <MdChevronRight size="24" />
+              </div>
+            ))
+          )}
+        </main>
+      </Table>
     </Container>
   );
 };
 
-export default Home;
+export default EmployeesList;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,88 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import { card } from '../../styles/shared';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-size: ${props => props.theme.sizes.heading};
+    font-weight: 700;
+    color: ${props => props.theme.colors.primary};
+    margin-bottom: 3rem;
+  }
+`;
+
+export const Table = styled.section`
+  ${card}
+  width: 100%;
+  max-width: 928px;
+
+  header,
+  main > div {
+    padding: 16px 24px;
+
+    display: grid;
+    grid-column-gap: 16px;
+    grid-template-columns: 280px 208px 128px 128px 24px;
+  }
+
+  header {
+    padding-bottom: 15px;
+    border-bottom: 1px solid ${props => props.theme.colors.light};
+
+    strong {
+      font-weight: 600;
+    }
+  }
+
+  main {
+    p {
+      margin-top: 2rem;
+      text-align: center;
+    }
+
+    > div {
+      border-radius: ${props => props.theme.radius};
+      cursor: pointer;
+      transition: 0.2s;
+      position: relative;
+
+      &:before {
+        position: absolute;
+        content: '';
+        top: 2px;
+        left: 2px;
+        width: 4px;
+        height: calc(100% - 4px);
+        border-radius: 4px;
+        transition: 0.2s;
+      }
+
+      &:hover {
+        background-color: ${props => props.theme.colors.light};
+
+        svg {
+          color: ${props => props.theme.colors.secondary};
+        }
+
+        &:before {
+          background: ${props => props.theme.colors.secondary};
+        }
+      }
+
+      p {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+
+      svg {
+        color: ${props => props.theme.colors.light_disabled};
+      }
+    }
+  }
+`;
